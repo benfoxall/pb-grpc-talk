5e56bb7f51c16907257e18d8906cca4c0ad41a7c
Ben Foxall <benfoxall@gmail.com>
2019-07-20 14:33:25 +0100

    Refactor PeerClient/PeerServer

--- examples/x-webrtcrpc-eroids/lib/host.js
+++ examples/x-webrtcrpc-eroids/lib/host.js
@@ -1,5 +1,71 @@
 import signalhub from "./signalhub.js"
-import Peer from './simplepeer.js';
+import Peer from './simplepeer.js'
+
+
+class PeerServer {
+
+  constructor (room) {
+    this.room = room
+    this.listeners = new Set
+    const peers = this.peers = new Map
+
+    var hub = signalhub('webrtcrpc-eroids', "https://signalhub-jccqtwhdwc.now.sh")
+
+    this.sub = hub.subscribe(room)
+
+    this.sub
+      .on('open', () => {
+        console.log("open…")
+      })
+      .on('data', (message) => {
+        console.log("GOT", message)
+
+        const remoteId = message.id;
+        const json = message.data;
+
+        if(!peers.has(remoteId)) {
+          const peer = new Peer({ initiator: false })
+          peer.on('error', err => console.log('error', err))
+
+          peer.on("signal", data => hub.broadcast(remoteId, data))
+
+          peer.on("connect", () => {
+            console.log("CONNECTED!!")
+          })
+
+          peer.on('close', () => {
+            console.log("CLOSED")
+            this.peers.delete(remoteId)
+          })
+
+          peer.on('data', data => {
+            this.listeners.forEach(fn => {
+              fn(remoteId, data);
+            })
+          })
+
+          peers.set(remoteId, peer);
+        }
+
+        peers.get(remoteId).signal(json);
+      })
+  
+
+  }
+
+  on(name, fn) {
+    if(name === 'data') {
+      this.listeners.add(fn)
+    }
+  }
+
+  send(id, payload) {
+    if(this.peers.has(id)) {
+      this.peers.get(id).send(payload)
+    }
+  }
+
+}
 
 export default (room) => {
   const main = document.querySelector('main')
@@ -11,45 +77,11 @@ export default (room) => {
     )
   )
 
-  const peers = new Map()
 
-  var hub = signalhub('webrtcrpc-eroids', "https://signalhub-jccqtwhdwc.now.sh")
+  const server = new PeerServer(room);
 
-
-  // TODO - SUPPORT AN ECHO TO AVOID RE-JOINING
-  hub.subscribe(room)
-    .on('data', (message) => {
-      console.log("GOT", message)
-
-      const remoteId = message.id;
-      const json = message.data;
-
-      if(!peers.has(remoteId)) {
-        const peer = new Peer({ initiator: false })
-        peer.on('error', err => console.log('error', err))
-
-        peer.on("signal", data => hub.broadcast(remoteId, data))
-
-        peer.on("connect", () => {
-          console.log("CONNECTED!!")
-        })
-
-        peer.on('close', () => {
-          console.log("CLOSED")
-        })
-
-        peer.on('data', data => console.log(data))
-
-
-        peers.set(remoteId, peer);
-      }
-
-      peers.get(remoteId).signal(json);
-
-      
-
-    })
-  
-
+  server.on("data", (id, data) => {
+    console.log("GOT DATA", id, data)
+  });
 
 }
--- examples/x-webrtcrpc-eroids/lib/join.js
+++ examples/x-webrtcrpc-eroids/lib/join.js
@@ -1,36 +1,53 @@
 import signalhub from "./signalhub.js"
 import Peer from './simplepeer.js';
 
+class PeerClient {
+  constructor(room) {
 
-const connect = async (room) => {
+    const connect = async (room) => {
 
-  // delay because live-reload
-  if(['127.0.0.1', 'localhost'].includes(document.location.hostname)) {
-    await new Promise(r => setTimeout(r, 500))
+      // delay for live-reload
+      if(['127.0.0.1', 'localhost'].includes(document.location.hostname)) {
+        await new Promise(r => setTimeout(r, 500))
+      }
+    
+      // the ID of this peer
+      const id = Math.random().toString(32)
+    
+      const hub = signalhub('webrtcrpc-eroids', "https://signalhub-jccqtwhdwc.now.sh")
+    
+      const subscription = hub.subscribe(id) 
+    
+      // Somehow this doesn't matter (and takes a long time)
+      // maybe it's waiting for a first message?
+      // await new Promise(resolve => subscription.on('open', resolve));
+    
+      const peer = new Peer({ initiator: true })
+    
+      peer.on('error', err => console.log('error', err))
+    
+      peer.on('signal', data =>  hub.broadcast(room, { id, data }))
+      subscription.on('data', d => peer.signal(d));
+    
+      await new Promise(resolve => peer.on("connect", resolve))
+    
+      hub.close();
+    
+      return peer;
+    }
+
+    this.peer = connect(room)
+
+    /* TODO - exit
+    window.addEventListener("beforeunload", () => {
+      this.send(new Uint8Array([44]))
+    })
+    */
   }
 
-  // the ID of this peer
-  const id = Math.random().toString(32)
-
-  const hub = signalhub('webrtcrpc-eroids', "https://signalhub-jccqtwhdwc.now.sh")
-
-  const subscription = hub.subscribe(id) 
-
-  // Somehow this doesn't matter (and takes a long time)
-  // await new Promise(resolve => subscription.on('open', resolve));
-
-  const peer = new Peer({ initiator: true })
-
-  peer.on('error', err => console.log('error', err))
-
-  peer.on('signal', data =>  hub.broadcast(room, { id, data }))
-  subscription.on('data', d => peer.signal(d));
-
-  await new Promise(resolve => peer.on("connect", resolve))
-
-  hub.close();
-
-  return peer;
+  send(payload) {
+    this.peer.then(p => p.send(payload))
+  }
 }
 
 
@@ -45,49 +62,10 @@ export default async (room) => {
   )
 
 
-  const peer = await connect(room);
-
-  console.log("YUP");
+  const client = new PeerClient(room)
 
   window.addEventListener('mousemove', (e) => {
-//screenX: 1196, screenY: 319
-    // console.log(e)
-
     const {screenX, screenY} = e;
-    peer.send(new Float32Array([screenX, screenY]))
-
+    client.send(new Uint8Array([screenX, screenY]))
   })
-
-  // var hub = signalhub('webrtcrpc-eroids', "https://signalhub-jccqtwhdwc.now.sh")
-
-  // var peer = new Peer({ initiator: true })
-
-  // peer.on('error', err => console.log('error', err))
-
-  // const id = Math.random().toString(32)
-
-  // // TODO WAIT ON SUBSCRIBE
-  // hub.subscribe(id)
-  // .on("open", () => {
-  //   console.log("openned")
-  // })
-  
-  //   .on("data", (data) => {
-  //   console.log("GOT", data)
-
-  //   peer.signal(data);
-  // })
-
-  // peer.on('signal', data => {
-  //   hub.broadcast(room, {
-  //     id,
-  //     data
-  //   })
-  // })
-
-  // peer.on("connect", () => {
-  //   console.log("CONNECTED!!")
-
-  //   hub.close();
-  // })
 }
